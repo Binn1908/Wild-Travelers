f391e9a6afe083d783bc6bc68cb35f1ed87eae65
Binn <126502493+Binn1908@users.noreply.github.com>
2023-06-13 18:25:42 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,13 +16,12 @@ import urllib.parse
 sl.set_page_config(layout = 'wide')
 
 #téléchargement du dataframe en cache
-@sl.cache_data   
+@sl.cache_data
 def load_df():    
     df1 = pd.read_pickle('df_final_pretraite_fr_1.pickle')
     df2 = pd.read_pickle('df_final_pretraite_fr_2.pickle')
     df3 = pd.read_pickle('df_final_pretraite_fr_3.pickle')
-    df4 = pd.read_pickle('df_final_pretraite_fr_4.pickle')
-    df = pd.concat([df1, df2, df3, df4])
+    df = pd.concat([df1, df2, df3])
     return df
 
 #téléchargement du modèle ML
@@ -46,29 +45,29 @@ df = load_df()
 #contenu dans le sidebar
 with sl.sidebar:
 
-	#logo dans le sidebar
+	#logo
 	logo = Image.open('logo.png')
 	sl.image(logo, width = 200)
 
-	#mise en place des filtres
+	#filtres
 
 	#adresse
 	with sl.form('address'):
 		user_address = sl.text_input('Adresse', value = '')
-		user_radius = sl.slider('Rayon (km)', min_value = 0, max_value = 10, value = 1, step = 1)
+		user_radius = sl.slider('Rayon (km)', min_value = 0, max_value = 10, value = 3, step = 1)
 
-		#sélection de la catégorie
+		#catégorie
 		type_options = ['Hébergement', 'Restauration']
 		user_type = sl.multiselect("Type d'établissement", type_options)
 
-		#checkbox mobilité
+		#mobilité
 		mobility = sl.checkbox('Accès mobilité réduite')
 
 		submit = sl.form_submit_button('Envoyer')
 
 	if submit:
 		if user_address == '':
-			sl.write('Aucune adresse renseignée')
+			sl.write('Veuillez renseigner une adresse.')
 
 	#sélection de la région
 	#region_options = df['region'].drop_duplicates().to_list()
@@ -100,9 +99,16 @@ with tab1:
 
 	#cartographie
 
-	df_filtered = df
+	#au lancement de l'appli, seuls les établissements de Paris sont affichés pour une meilleure performance
+	df_filtered = df.loc[df.departement == 'Paris']
+
+	lat_default = 48.856578
+	lon_default = 2.351828
 
 	if submit:
+
+		#seulement quand les filtres sont appliqués, l'appli affiche (selon l'adresse) d'autres régions
+		df_filtered = df
 
 		if bool(user_type) == True:
 			df_filtered = df_filtered.loc[df_filtered['category'].isin(user_type)]
@@ -110,6 +116,7 @@ with tab1:
 		if bool(mobility) == True:
 			df_filtered = df_filtered.loc[df_filtered['reducedMobilityAccess'] == True]
 
+		#récupérer coordonnées de l'adresse via API
 		if user_address != '':
 			url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(user_address) + '?format=json'
 			response = requests.get(url).json()
@@ -117,8 +124,11 @@ with tab1:
 			user_lon = float(response[0]["lon"])
 			user_loc = (user_lat, user_lon)
 
+			#possible de trouver la ville à partir des coordonnées trouvées ? Si oui, filtrer df par la ville pour réduire le calcul de la distance pour chaque établissement ci-dessous
+
 			list_index = []
 
+			#calculer la distance entre l'adresse de l'utilisateur et chaque établissement
 			for etablissement in range(len(df_filtered)):
 				latitude = float(df_filtered.iloc[etablissement].latitude)
 				longitude = float(df_filtered.iloc[etablissement].longitude)
@@ -131,24 +141,19 @@ with tab1:
 
 			df_filtered = df_filtered.iloc[list_index]
 
+			if len(df_filtered) > 0:
+				sl.write(f"Nombre d'établissements trouvés : {len(df_filtered)}")
+
+				lat_default = df_filtered['latitude'].mean()
+				lon_default = df_filtered['longitude'].mean()
+
+			else:
+				sl.write("Aucun établissement trouvé")
+
 	#df_filtered = df.loc[(df['region'] == user_region) & (df['departement'] == user_dep) &
 	#(df['ville'] == user_ville)]
 
 	df_coordinates = df_filtered[['nom_etablissement', 'category', 'latitude', 'longitude']]
-	
-	sl.write(f"Nombre d'établissements trouvés : {len(df_coordinates)}")
-
-	#s'il y a au moins un établissement dans notre recherche, recentrer la carte sur la position moyenne de tous les points
-	if len(df_coordinates) > 0:
-		lat_default = df_coordinates['latitude'].mean()
-		lon_default = df_coordinates['longitude'].mean()
-	#s'il y a aucun résultat, recentrer la carte sur Paris
-	else:
-		#coordonnées de Paris
-		lat_default = 48.856578
-		lon_default = 2.351828
-
-	#initial_view_state dans le code suivant indique la vue initiale de la carte en prenant en compte les coordonnées définies ci-dessus
 
 	map_main = sl.pydeck_chart(pdk.Deck(
 	    map_style='road',
@@ -277,9 +282,9 @@ with tab3:
 
 	preprocessed_text = preprocess_text(new_text)
 
-	df = df.dropna(subset = ['description_pretraitee', 'category'])
+	#df = df.dropna(subset = ['description_pretraitee', 'category'])
 
-	df.reset_index(drop=True, inplace=True)
+	#df.reset_index(drop=True, inplace=True)
 
 	X = df['description_pretraitee']
 	y = df['category']
