6e57b521c499e1c1ef10f83b981226ab640ddf66
Binn <126502493+Binn1908@users.noreply.github.com>
2023-06-14 16:41:04 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -22,17 +22,38 @@ def load_df():
     df = pd.concat([df1, df2, df3, df4])
     return df
 
-#téléchargement du modèle ML
-def load_model():
-    with open('mdl.pkl', 'rb') as file:
-        model = pickle.load(file)
-    return model
-
 #téléchargement du vectorizer
-def load_vectorizer():
-    with open('vectorizer.pkl', 'rb') as file:
-        vectorizer = pickle.load(file)
-    return vectorizer
+#def load_vectorizer():
+#    with open('vectorizer.pkl', 'rb') as file:
+#        vectorizer = pickle.load(file)
+#    return vectorizer
+
+#téléchargement du modèle ML
+#def load_model():
+#    with open('mdl.pkl', 'rb') as file:
+#        model = pickle.load(file)
+#    return model
+
+@sl.cache_data
+def load_ml():
+	#df = df.dropna(subset = ['description_pretraitee', 'category'])
+	#df.reset_index(drop=True, inplace=True)
+
+	X = df['description_pretraitee']
+	y = df['category']
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+	vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1, 2), min_df=5)
+
+	X_train_vectorized = vectorizer.fit_transform(X_train)
+	#X_test_vectorized = vectorizer.transform(X_test)
+
+	model = LogisticRegression(class_weight='balanced', max_iter=1000)
+
+	model.fit(X_train_vectorized, y_train)
+
+	return vectorizer, model
 
 df = load_df()
 
@@ -55,7 +76,7 @@ with sl.sidebar:
 
 	#mobilité
 	mobility = sl.checkbox('Accès mobilité réduite')
-	
+
 	#région
 	region_options = df['region'].drop_duplicates().to_list()
 	#mettre en première position l'IDF dans la liste des options
@@ -95,7 +116,8 @@ with tab1:
 	if bool(mobility) == True:
 		df_filtered = df_filtered.loc[df_filtered['reducedMobilityAccess'] == True]
 
-	df_coordinates = df_filtered[['nom_etablissement', 'category', 'latitude', 'longitude']]
+	df_coordinates = df_filtered[['nom_etablissement', 'category', 'latitude', 'longitude', 'rue', 
+		'code_postal', 'ville', 'telephone', 'email', 'site_web']]
 
 	if len(df_coordinates) > 0:
 		
@@ -133,7 +155,7 @@ with tab1:
         	),
     	],
     	tooltip={
-	        'html': '<b>Nom : </b> {nom_etablissement} <br/> <b>Type d\'établissement : </b> {category} <br/>',
+	        'html': '<b>Nom : </b> {nom_etablissement} <br/> <b>Type d\'établissement : </b> {category} <br/> <b>Adresse : </b> {rue}, {code_postal} {ville} <br/> <b>Téléphone : </b> {telephone} <br/> <b>Email : </b> {email} <br/> <b>Site web : </b> {site_web} <br/>',
 	        'style': {
 	        	'color': 'white'}}
 	))
@@ -233,36 +255,20 @@ with tab2:
 #tab Robot ML
 with tab3:
 
-	sl.write("Test du modèle")
+	sl.markdown("<h3 style='text-align: center;'>Test du modèle</h3>", unsafe_allow_html=True)
 
 	example_text = "Nous vous proposons une cuisine moderne avec une belle vue des montagnes."
-	new_text = sl.text_area("Veuillez renseigner une description", value = example_text)
+	new_text = sl.text_area("Veuillez renseigner une description :", value = example_text)
 
 	preprocessed_text = preprocess_text(new_text)
 
-	#df = df.dropna(subset = ['description_pretraitee', 'category'])
-
-	#df.reset_index(drop=True, inplace=True)
-
-	X = df['description_pretraitee']
-	y = df['category']
-
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-	vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1, 2), min_df=5)
-
-	X_train_vectorized = vectorizer.fit_transform(X_train)
-	X_test_vectorized = vectorizer.transform(X_test)
-
-	model = LogisticRegression(class_weight='balanced', max_iter=1000)
-
-	model.fit(X_train_vectorized, y_train)
+	vectorizer, model = load_ml()
 
 	new_text_vectorized = vectorizer.transform([preprocessed_text])
 
 	predicted_category = model.predict(new_text_vectorized)
 
-	sl.write(f"Prédiction de catégorie : {predicted_category[0]}")
+	sl.write(f"Prédiction du type d'établissement : {predicted_category[0]}")
 
 #footer
 sl.divider()
