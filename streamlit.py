ce6daf064827a0ff5b9e673834bf4b3569397d24
Binn <126502493+Binn1908@users.noreply.github.com>
2023-06-26 14:54:07 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -55,19 +55,6 @@ def load_ml():
 
 	return vectorizer, model
 
-def open_website(link):
-    webbrowser.open(link)
-
-def set_tooltip(nom_etablissement, site_web):
-    map_main.deck_widget.tooltip = {
-        'html': '<b>Nom : </b> {nom_etablissement} <br/> <b>Type d\'établissement : </b> {category} <br/> <b>Adresse : </b> {rue}, {code_postal} {ville} <br/> <b>Téléphone : </b> {telephone} <br/> <b>Email : </b> {email} <br/> <b>Site web : </b> <a href="{site_web}" target="_blank">{site_web}</a><br/>',
-        'style': {
-            'color': 'white'
-        }
-    }
-    if site_web:
-        open_website(site_web)
-
 df = load_df()
 
 #model = load_model()
