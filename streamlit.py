7fdb4c9fd42f25f7ee2b9797b4755cadd79594c1
Binn <126502493+Binn1908@users.noreply.github.com>
2023-06-27 14:55:01 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,10 +9,6 @@ import seaborn as sns
 import streamlit as sl
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-import ipywidgets as widgets
-import geopandas as gpd
-import sidecar
-import webbrowser
 
 sl.set_page_config(layout = 'wide')
 
@@ -58,19 +54,6 @@ def load_ml():
 	model.fit(X_train_vectorized, y_train)
 
 	return vectorizer, model
-
-def open_website(link):
-    webbrowser.open(link)
-
-def set_tooltip(nom_etablissement, site_web):
-    map_main.deck_widget.tooltip = {
-        'html': '<b>Nom : </b> {nom_etablissement} <br/> <b>Type d\'établissement : </b> {category} <br/> <b>Adresse : </b> {rue}, {code_postal} {ville} <br/> <b>Téléphone : </b> {telephone} <br/> <b>Email : </b> {email} <br/> <b>Site web : </b> <a href="{site_web}" target="_blank">{site_web}</a><br/>',
-        'style': {
-            'color': 'white'
-        }
-    }
-    if site_web:
-        open_website(site_web)
 
 df = load_df()
 
@@ -195,7 +178,6 @@ with tab1:
     			size_scale=15,
    				get_position=["longitude", "latitude"],
     			pickable=True,
-			#onClick="setTooltip({nom_etablissement}, {site_web})"
         	),
     	],
     	tooltip={
