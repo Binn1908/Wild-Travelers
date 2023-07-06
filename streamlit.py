bb842c50c6f39c6f5cde66a86c13142751d04eb4
Binn <126502493+Binn1908@users.noreply.github.com>
2023-07-06 15:23:55 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -283,19 +283,18 @@ with tab3:
 
 	vectorizer1, model1 = load_ml1()
 
+	vectorizer2, model2 = load_ml2()
+
 	new_text_vectorized1 = vectorizer1.transform([preprocessed_text])
 
 	predicted_category1 = model1.predict(new_text_vectorized1)
 
-	sl.write(f"Prédiction du type d'établissement : {predicted_category1[0]} (probabilité : {model1.predict_proba(new_text_vectorized1)})")
-	
-	#vectorizer2, model2 = load_ml2()
-
-	#new_text_vectorized = vectorizer2.transform([preprocessed_text])
-
-	#predicted_category = model2.predict(new_text_vectorized)
-
-	#sl.write(f"Prédiction du type d'établissement : {predicted_category[0]} (probabilité : {model2.predict_proba(new_text_vectorized)})")
+	if predicted_category1 == 'Autres':
+		sl.write(f"Prédiction du type d'établissement : {predicted_category1[0]} (probabilité : {model1.predict_proba(new_text_vectorized1)})")
+	else:
+		new_text_vectorized2 = vectorizer2.transform([preprocessed_text])
+		predicted_category2 = model2.predict(new_text_vectorized2)
+		sl.write(f"Prédiction du type d'établissement : {predicted_category2[0]} (probabilité : {model2.predict_proba(new_text_vectorized2)})")
 
 #footer
 sl.divider()
