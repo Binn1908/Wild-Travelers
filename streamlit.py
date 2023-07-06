48c0c46cead3d8074cac7beb2ac9e2e4ac617f37
Binn <126502493+Binn1908@users.noreply.github.com>
2023-07-06 15:08:27 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -285,7 +285,7 @@ with tab3:
 
 	predicted_category1 = model1.predict(new_text_vectorized1)
 
-	sl.write(f"Prédiction du type d'établissement : {predicted_category1[0]} (probabilité : {model1.predict_proba(new_text_vectorized)})")
+	#sl.write(f"Prédiction du type d'établissement : {predicted_category1[0]} (probabilité : {model1.predict_proba(new_text_vectorized1)})")
 	
 	#vectorizer2, model2 = load_ml2()
 
